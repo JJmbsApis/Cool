0518ec4e942bd776128b9c08bbd11f531c0d510a
JJmbsApis <104864998+JJmbsApis@users.noreply.github.com>
2023-02-19 14:33:17 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
--- Jmbs
+-- Gui to Lua
+-- Version: 3.2
 
 -- Instances:
 
 local Turioasaid = Instance.new("ScreenGui")
 local Other = Instance.new("ImageLabel")
+local TextButton = Instance.new("TextButton")
 local Frame = Instance.new("Frame")
 local TextLabel = Instance.new("TextLabel")
 local ImageButton = Instance.new("ImageButton")
@@ -24,6 +26,18 @@ Other.ImageColor3 = Color3.fromRGB(34, 34, 34)
 Other.ScaleType = Enum.ScaleType.Slice
 Other.SliceCenter = Rect.new(100, 100, 100, 100)
 Other.SliceScale = 0.120
+
+TextButton.Parent = Other
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 1.000
+TextButton.Position = UDim2.new(0, 0, 0.17318435, 0)
+TextButton.Size = UDim2.new(0, 125, 0, 50)
+TextButton.Font = Enum.Font.Unknown
+TextButton.Text = "AutoFarm: OFF"
+TextButton.TextColor3 = Color3.fromRGB(136, 255, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
 
 Frame.Parent = Turioasaid
 Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -51,11 +65,33 @@ ImageButton.Image = "http://www.roblox.com/asset/?id=11560341824"
 
 -- Scripts:
 
-local function LMUB_fake_script() -- ImageButton.LocalScript 
+local function XTAHBXG_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		if script.Parent.Text == "AutoFarm: OFF" then
+			script.Parent.Text = "AutoFarm: ON"
+			repeat
+				game.Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(28.635387420654297, 204.80914306640625, -39.55120849609375))
+				wait(5)
+				game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-999373.03125, -195.43666076660156, -41.42085266113281))
+				wait(5)
+				game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-1500373.03125, -195.43666076660156, -41.42085266113281))
+				wait(2)
+				game.Players.LocalPlayer.Character.Humanoid.Health = 0
+				wait(6)
+			until script.Parent.Text == "AutoFarm: OFF"
+		else
+			script.Parent.Text = "AutoFarm: OFF"
+		end
+	end)
+end
+coroutine.wrap(XTAHBXG_fake_script)()
+local function NYBOKFY_fake_script() -- ImageButton.LocalScript 
 	local script = Instance.new('LocalScript', ImageButton)
 
 	script.Parent.MouseButton1Click:Connect(function()
 		script.Parent.Parent.Parent.Other.Visible = not script.Parent.Parent.Parent.Other.Visible
 	end)
 end
-coroutine.wrap(LMUB_fake_script)()
+coroutine.wrap(NYBOKFY_fake_script)()
