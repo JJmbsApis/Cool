a6b7b09d4d9f9cdde73191004295c565ee10a432
JJmbsApis <104864998+JJmbsApis@users.noreply.github.com>
2023-02-19 14:25:25 -0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,61 @@
+-- Jmbs
+
+-- Instances:
+
+local Turioasaid = Instance.new("ScreenGui")
+local Other = Instance.new("ImageLabel")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local ImageButton = Instance.new("ImageButton")
+
+--Properties:
+
+Turioasaid.Name = "Turioasaid"
+Turioasaid.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+Other.Name = "Other"
+Other.Parent = Turioasaid
+Other.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Other.BackgroundTransparency = 1.000
+Other.Position = UDim2.new(0.118990384, 0, 0.0424592383, 0)
+Other.Size = UDim2.new(0, 125, 0, 179)
+Other.Image = "rbxassetid://3570695787"
+Other.ImageColor3 = Color3.fromRGB(34, 34, 34)
+Other.ScaleType = Enum.ScaleType.Slice
+Other.SliceCenter = Rect.new(100, 100, 100, 100)
+Other.SliceScale = 0.120
+
+Frame.Parent = Turioasaid
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.Position = UDim2.new(0.118990384, 0, 0.0424592383, 0)
+Frame.Size = UDim2.new(0, 125, 0, 25)
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Size = UDim2.new(0, 102, 0, 25)
+TextLabel.Font = Enum.Font.SourceSansBold
+TextLabel.Text = "RandomHub"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+ImageButton.Parent = Frame
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BackgroundTransparency = 1.000
+ImageButton.Position = UDim2.new(0.815999985, 0, 0, 0)
+ImageButton.Size = UDim2.new(0, 23, 0, 25)
+ImageButton.Image = "http://www.roblox.com/asset/?id=11560341824"
+
+-- Scripts:
+
+local function LMUB_fake_script() -- ImageButton.LocalScript 
+	local script = Instance.new('LocalScript', ImageButton)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.Parent.Other.Visible = not script.Parent.Parent.Parent.Other.Visible
+	end)
+end
+coroutine.wrap(LMUB_fake_script)()
